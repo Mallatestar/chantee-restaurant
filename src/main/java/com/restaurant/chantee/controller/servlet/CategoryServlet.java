d1b89d3bef6ff3b76c818e1b1c5d88a60216d76c
VladTsyupa <bengijha@gmail.com>
2021-02-01 13:29:03 +0200

    Frontend on jsp

---
+++ src/main/java/com/restaurant/chantee/controller/servlet/CategoryServlet.java
@@ -0,0 +1,22 @@
+package com.restaurant.chantee.controller.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CategoryServlet", value = "/CategoryServlet")
+public class CategoryServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
